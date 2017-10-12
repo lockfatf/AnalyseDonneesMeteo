aff761b110706ad03a84166973a12624c510869e
Axel <axel3392@hotmail.fr>
2017-10-12 10:52:46 +0200

    qqs comms en plus

--- Code/nettoyage.R
+++ Code/nettoyage.R
@@ -7,11 +7,11 @@ library(factoextra)
 climat <- fread("climat.201708.csv",data.table = F)
 # Récupérer variables avec plus de 10% de NA
 idx_bad_col <- which(apply(climat,2,function(x){sum(is.na(x))})> (dim(climat)[1]/10))
-# et les enlever
+# et les enlever ainsi que FXAB qui doit être mise de côté
 climat_rm_na <- climat[,-c(idx_bad_col,which(colnames(climat)=="FXAB"))]
-
+# Virer lignes avec au moins 1 NA
 idx_bad_row <-  which(apply(climat_rm_na,1,function(x){sum(is.na(x))}) > 1)
-# On retrouve 9 lignes avec des NA, on peut récupérer un tableau de 49 lignes au lieu de 58 sans aucun NA!
+# On retrouve 9 lignes avec au moins 1 NA, on récupère un tableau de 49 lignes sans aucun NA!
 climat_rm_na <- climat_rm_na[-idx_bad_row,]
 
 # ACP test pré-exploratoire
