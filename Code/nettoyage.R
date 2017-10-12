9581a0279c6d3ac6034e35ae1d05b995b2078dc6
Axel <axel3392@hotmail.fr>
2017-10-12 11:24:44 +0200

    viré les variables avec variance trop faible pour la pca

--- Code/nettoyage.R
+++ Code/nettoyage.R
@@ -3,13 +3,16 @@ library(tidyverse)
 library(FactoMineR)
 library(factoextra)
 library(rgl)
+library(caret)
 
 # Chargement des données avec fread, plus intelligent que read.csv
 climat <- fread("climat.201708.csv",data.table = F)
-# Récupérer variables avec plus de 10% de NA
+# Récupérer variables avec plus de 10% de NA pour les enlever
 idx_bad_col <- which(apply(climat,2,function(x){sum(is.na(x))})> (dim(climat)[1]/10))
+# elever les variables avec variance trop faible, problème pour l'acp sinon
+near_zero_var <- nzv(climat, freqCut = 90/10)
 # et les enlever ainsi que FXAB qui doit être mise de côté
-climat_rm_na <- climat[,-c(idx_bad_col,which(colnames(climat)=="FXAB"))]
+climat_rm_na <- climat[,-c(idx_bad_col,near_zero_var,which(colnames(climat)=="FXAB"))]
 # Virer lignes avec au moins 1 NA
 idx_bad_row <-  which(apply(climat_rm_na,1,function(x){sum(is.na(x))}) > 0)
 # On retrouve 9 lignes avec au moins 1 NA, on récupère un tableau de 46 lignes sans NA
@@ -17,3 +20,4 @@ climat_rm_na <- climat_rm_na[-idx_bad_row,]
 
 # ACP test pré-exploratoire
 res_pca <- PCA(climat_rm_na %>% select_if(is.numeric), ncp = 30, scale.unit = TRUE, graph=T)
+plot3d(res_pca$ind$coord[,1:3])
